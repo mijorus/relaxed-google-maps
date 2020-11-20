587522bfc9b0318a7c9c5cc7e35d2856be92cd2f
mijorus <lorenzo.mijorus@gmail.com>
2020-11-20 18:59:18 +0100

    Merge branch 'master' of https://github.com/mijorus/relaxed-google-maps

--- public/js/examples.js
+++ public/js/examples.js
@@ -1,5 +1,5 @@
 // Real world example
-import RelaxedMap from './js/relaxedGMap.js'
+import RelaxedMap from './relaxedGMap.js'
 document.addEventListener('DOMContentLoaded', function () 
 {
     const location = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158858.182370726!2d-0.10159865000000001!3d51.52864165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondra%2C%20Regno%20Unito!5e0!3m2!1sit!2sit!4v1605344840356!5m2!1sit!2sit'
